3ac4bed22730ce1338c6f3c485d848427aa86db3
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-02-14 23:15:58 -0600

    ompareTo implemented

--- Event.cpp
+++ Event.cpp
@@ -4,6 +4,12 @@ Event::Event(int theTime, Simulation *theSim) :eventTime(theTime), sim (theSim) 
 Event::~Event() {}
 int Event::getTime(){ return eventTime; }
 
-int Event::compareTo(OrderedItem *other){
-	return -1; // you must implement this.
+// compares the eventTime of two events
+// Returns: 0 if the eventTimes are equal
+//          +ve number if the first eventTime is later than the second one
+//          -ve number if the second eventTime is later than the first one
+int Event::compareTo(OrderedItem *other)
+{
+    Event *myEvent = dynamic_cast<Event *>(other);
+	return ((this->eventTime) - (myEvent->eventTime));
 }// compareTo
