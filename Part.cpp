f382bacb1425886c61e5808a60babf522c128313
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-02-16 02:18:17 -0600

    Fields and constructor implemented.

--- Part.cpp
+++ Part.cpp
@@ -1,1 +1,9 @@
 #include "Part.h"
+using namespace std;
+
+Part::Part(int newPartNum, int newTime) :partNumber(newPartNum), arrivalTime(newTime), departTime(0), waitTime(0){}
+
+void Part::print()
+{
+
+}// print
--- Part.h
+++ Part.h
@@ -1,5 +1,16 @@
 #pragma once
 
-class Part {
+#include "ListItem.h"
 
+class Part: public ListItem
+{
+private:
+    int partNumber; // what part is it? 0,1 or 2?
+    int arrivalTime; // when will the part arrive?
+    int departTime; // when is the part going to depart?
+    int waitTime; // how long a part waited in the queue before assembly started
+public:
+    Part(int, int);
+    void print();
+    ~Part();
 };// class Part
