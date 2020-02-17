de462c2bcdbc010631bc4b1bc3acf8e2a8ab9841
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-02-17 02:15:18 -0600

    Constructor and print statement added.

--- ProductArrival.cpp
+++ ProductArrival.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 #include "ProductArrival.h"
+#include "Part.h"
+using namespace std;
+
+ProductArrival::ProductArrival(int theTime, Simulation *sim, Part *thePart) : Event(theTime, sim), myPart(thePart) {}
+
+// todo:
+void ProductArrival::processEvent()
+{
+
+}// processEvent
+
+void ProductArrival::print()
+{
+    cout << "At time " << getTime() << " product arrives to be assembled by Finishing Station." << endl;
+}// print
+
+ProductArrival::~ProductArrival() {}
--- ProductArrival.h
+++ ProductArrival.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "Event.h"
+class Part;
 
 class ProductArrival: public Event
 {
+private:
+    Part *myPart;
 public:
-    ProductArrival();
+    ProductArrival(int, Simulation *, Part *);
     ~ProductArrival();
     void processEvent();
+    void print();
 };// class ProductArrival
